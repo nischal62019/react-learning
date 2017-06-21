f4397abd6af2b1401f1e3a2e4cb17e68f1c2f6b4
Nischal <nischal.srinivas@gmail.com>
2017-06-21 19:25:56 +0530

    fixed the app title

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Header extends React.Component {
     render() {
         return (<h1>
-            Todo App
+            My awesome app
         </h1>)
     }
 }
